57e02da08569c1b158cf0f5913c91856ade833cd
bsafwene <baroudi.safwene@gmail.com>
2015-03-23 13:10:43 +0100

    Almost finished.

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "cTc.h"
+
+Coordinates chat ;
+Map map ;
+main()
+{
+    chat.x = MAP_SIZE/2;
+    chat.y = MAP_SIZE/2;
+    initializeMap(&map,&chat);
+    draw(&map);
+    while( !gameOver(&map, &chat) )
+    {
+        chaseCat(&map);
+        draw(&map);
+        if(existWay(&chat,&map))
+            moveCat(&map, &chat);
+        draw(&map);
+    }
+    if(chat.x ==0 || chat.y == 0 || chat.x == MAP_SIZE - 1 ||  chat.y == MAP_SIZE-1 )
+        printf("The cat has escaped! You Lose\n");
+    else
+        printf("Well done, You catched the cat!\n");
+    quitGame(&map);
+}
